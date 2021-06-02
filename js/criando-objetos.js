ea691567a1308052caa021395ce2e1ef3ac8b12e
Curvaderioou <felipedegenerportella@yahoo.com>
2021-06-01 21:22:40 -0300

    explorando criacao de objetos e prototipos

--- js/criando-objetos.js
+++ js/criando-objetos.js
@@ -27,7 +27,7 @@ Centauro.qtdadePernas = 4;
 Centauro.qtdadeBracos = 2;
 
 */
-
+/*
 function Shining(){
   Shining.prototype.qtdpernas = 0
   Shining.prototype.qtdbracos = 0
@@ -45,11 +45,72 @@ Heroe.prototype.tanker = function(){
 
 const Centauro = new Heroe(false);
 
+*/
+/*
+const carro = {
+marca: "Marca",
+preco: 0,
+}
 
+const honda = carro;
 
+honda.marca = "honda"
+honda.preco = 2000
 
+console.log(carro.marca)
+console.log(carro.preco)
 
+const fiat = carro;
 
- 
+fiat.marca = "fiat"
+fiat.preco = 1500
 
+console.log(carro.marca)
+console.log(carro.preco)
+
+*/
+
+function Carro(){
+  this.marca = "Marca";
+  this.preco = 0
+};
+
+const honda = new Carro();
+
+honda.marca = "Honda"
+honda.preco = 3000
+
+console.log(honda.marca, honda.preco)
   
+const fiat = new Carro();
+
+fiat.marca = "FIAT";
+fiat.preco = 1100
+
+console.log(fiat.marca, fiat.preco);
+
+console.log(Carro)
+
+/*
+O new cria um novo objeto baseado no nome que eu dei, por exemplo 
+honda = new carro, entao ele vai criar um novo objeto vazio com o nome honda se baseando no seu construtor que é o Carro.
+depois ele define os prototipos, ou seja,
+honda.prototype = Carro.prototype, ou seja o honda vai herdar os prototipos de Carro.
+Após isso ele vai apontar o this para o honda.
+Após isso ele vai executar a função, substituindo o this pelo objeto, o que era this.marca agora é honda.marca, o que era this.preco agora é honda.preco.
+e por fim ele retorna o novo objeto, honda ={
+  marca:"Marca"
+  preco:"0"
+}
+*/
+
+//podemos entao criar parametros 
+
+function Cachorro (raca, preco){
+  this.raca = raca;
+  this.preco = preco;
+}
+
+const cachorroPequeno = new Cachorro("viralata", 4000);
+const cachorroMedio = new Cachorro("boxer", 6000);
+const cachorroGrande = new Cachorro("Pastor", 14000);
